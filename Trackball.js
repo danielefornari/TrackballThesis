270bef1eae965052bb6ab7e128789179fc0c720e
Soulhikari <78259633+Soulhikari@users.noreply.github.com>
2021-01-30 18:10:51 +0100

    Update Trackball.js

--- Trackball.js
+++ Trackball.js
@@ -1,6 +1,8 @@
 import * as THREE from 'https://unpkg.com/three/build/three.module.js';
 
 const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 function sayHi() {
     alert("Hi");
