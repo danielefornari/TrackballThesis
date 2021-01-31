b00d6a3bf910a72c4f8a2d3072435f2c933b124b
Soulhikari <78259633+Soulhikari@users.noreply.github.com>
2021-01-31 13:06:03 +0100

    Update Trackball.js

--- Trackball.js
+++ Trackball.js
@@ -1,4 +1,5 @@
 import * as THREE from 'https://unpkg.com/three/build/three.module.js';
 
-const canvas = document.querySelector('#myCanvas');
-const renderer = new THREE.WebGLRenderer({canvas});
+//const canvas = document.querySelector('#myCanvas'); //riferimento al canvas
+const canvas = document.getElementById("myCanvas");
+const renderer = new THREE.WebGLRenderer({canvas}); //istanzio il renderer dicendo che lo voglio nel canvas che gli passo
