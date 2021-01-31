f74e88505a8cc12651b9478c01586e86678ebb51
Soulhikari <78259633+Soulhikari@users.noreply.github.com>
2021-01-31 13:01:44 +0100

    Update Trackball.js

--- Trackball.js
+++ Trackball.js
@@ -1,27 +1,4 @@
 import * as THREE from 'https://unpkg.com/three/build/three.module.js';
 
-const renderer = new THREE.WebGLRenderer(document.getElementById("myCanvas"));
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
-camera.position.set( 0, 0, 100 );
-camera.lookAt( 0, 0, 0 );
-
-const scene = new THREE.Scene();
-
-const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
-
-
-const points = [];
-points.push( new THREE.Vector3( - 10, 0, 0 ) );
-points.push( new THREE.Vector3( 0, 10, 0 ) );
-points.push( new THREE.Vector3( 10, 0, 0 ) );
-
-const geometry = new THREE.BufferGeometry().setFromPoints( points );
-
-const line = new THREE.Line( geometry, material );
-
-
-scene.add( line );
-renderer.render( scene, camera );
+const canvas = document.querySelector('#myCanvas');
+const renderer = new THREE.WebGLRenderer({canvas});
