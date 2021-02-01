08821dad2bb080c41d4ceb8007f07ee7b850de42
Soulhikari <78259633+Soulhikari@users.noreply.github.com>
2021-02-01 13:15:47 +0100

    Update Trackball.js

--- Trackball.js
+++ Trackball.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three';
-import {GLTFLoader} from 'three.examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from './three';
+import {GLTFLoader} from './three.examples/jsm/loaders/GLTFLoader.js';
 
 //const canvas = document.querySelector('#myCanvas'); //riferimento al canvas
 const canvas = document.getElementById("myCanvas");
