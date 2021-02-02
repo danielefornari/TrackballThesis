6b4149d390bb43ad77c056a82189c094a82ba74c
Soulhikari <78259633+Soulhikari@users.noreply.github.com>
2021-02-03 00:45:33 +0100

    Update Trackball.js

--- Trackball.js
+++ Trackball.js
@@ -17,14 +17,14 @@ let cursorData = {
         x:0,
         y:0,
         toVector3: function() {
-            return new THREE.Vector3(this.x, this.y, 2).multiplyScalar(10);
+            return new THREE.Vector3(this.x, this.y, 0.1).multiplyScalar(20);
         }
     },
     prev: {
         x:0,
         y:0,
         toVector3: function() {
-            return new THREE.Vector3(this.x, this.y, 2).multiplyScalar(10);
+            return new THREE.Vector3(this.x, this.y, 0.1).multiplyScalar(20);
         }
     },
     updateCursorPositions: function(x, y, canvas) {
