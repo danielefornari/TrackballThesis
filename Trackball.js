84518eb769ee58bb201225762002524ad76a36b6
Soulhikari <78259633+Soulhikari@users.noreply.github.com>
2021-02-03 16:52:40 +0100

    Update Trackball.js

--- Trackball.js
+++ Trackball.js
@@ -75,7 +75,7 @@ const near = 0.1
 const far = 5;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 
-camera.position.z = -2;
+camera.position.z = 2;
 
 //scene
 const scene = new THREE.Scene();
