1ea3669193b7b2a0de57843e2d3332576811fda2
Soulhikari <78259633+Soulhikari@users.noreply.github.com>
2021-02-03 21:25:57 +0100

    Update Trackball.js

--- Trackball.js
+++ Trackball.js
@@ -27,7 +27,7 @@ function updateCursorPosition(x, y) {
     //coordinate x/y del cursore rispetto al canvas con valori tra [-1, 1]
     currentCursorPosition.setX(((x - canvasRect.left) / canvasRect.width) * 2 - 1);
     currentCursorPosition.setY(((y - canvasRect.top) / canvasRect.height) * 2 - 1);
-    currentCursorPosition.unproject(camera);
+    //currentCursorPosition.unproject(camera);
     currentCursorPosition.setZ(unprojectZ(currentCursorPosition.x, currentCursorPosition.y));
 };
 
