e1980c7241f9ad721e2bae0aeb80323bbfd51415
Soulhikari <78259633+Soulhikari@users.noreply.github.com>
2021-02-04 16:22:59 +0100

    Update Trackball.js

--- Trackball.js
+++ Trackball.js
@@ -1,6 +1,6 @@
-import * as THREE from 'https://unpkg.com/three/build/three.module.js';
+//import * as THREE from 'https://unpkg.com/three/build/three.module.js';
 
-//import * as THREE from 'three';
+import * as THREE from 'three';
 
 const canvas = document.getElementById("myCanvas");
 const rotationAxisParagraph = document.getElementById("rotationAxisParagraph");
@@ -16,16 +16,21 @@ const raycaster = new THREE.Raycaster();
 
 let tracking = false;   //indica se sto eseguendo il tracking del cursore del mouse
 let currentCursorPosition = new THREE.Vector3();    //posizione corrente del cursore
-let prevCursorPosition = new THREE.Vector3();   //posizione precedente del cursore
+let startCursorPosition = new THREE.Vector3();   //posizione iniziale del cursore
 let rotationAxis = new THREE.Vector3(); //asse di rotazione
+let quatState;
 
-function updateCursorPosition(x, y) {
-    prevCursorPosition.copy(currentCursorPosition);
+function getCursorPosition(x, y) {
+    //prevCursorPosition.copy(currentCursorPosition);
     let canvasRect = canvas.getBoundingClientRect();
 
     //coordinate x/y del cursore rispetto al canvas con valori tra [-1, 1]
-    currentCursorPosition.setX(((x - canvasRect.left) / canvasRect.width) * 2 - 1);
-    currentCursorPosition.setY(((y - canvasRect.top) / canvasRect.height) * 2 - 1);
+    let cursorPosition = new THREE.Vector3();
+    cursorPosition.setX(((x - canvasRect.left) / canvasRect.width) * 2 - 1);
+    cursorPosition.setY(((y - canvasRect.top) / canvasRect.height) * 2 - 1);
+    cursorPosition.setZ(unprojectZ(cursorPosition.x, cursorPosition.y));
+    return cursorPosition;
+
     //currentCursorPosition.unproject(camera);
 
     /*let v = new THREE.Vector2(currentCursorPosition.x, currentCursorPosition.y);
@@ -36,7 +41,7 @@ function updateCursorPosition(x, y) {
         currentCursorPosition.copy(intersect[0].point);
         currentCursorPosition.setZ(unprojectZ(currentCursorPosition.x, currentCursorPosition.y));
     }*/
-    currentCursorPosition.setZ(unprojectZ(currentCursorPosition.x, currentCursorPosition.y));
+    //currentCursorPosition.setZ(unprojectZ(currentCursorPosition.x, currentCursorPosition.y));
 };
 
 //camera
@@ -103,20 +108,21 @@ function mouseUpListener() {
 function mouseDownListener(event) {
     //cursorScreenPosition.x = event.clientX;
     //cursorScreenPosition.y = event.clientY;
-    updateCursorPosition(event.clientX, event.clientY);
+    cube.quaternion.copy(quatState);
+    startCursorPosition = getCursorPosition(event.clientX, event.clientY);
     tracking = true;
 };
 
 function mouseMoveListener(event) {
     if(tracking) {
-        updateCursorPosition(event.clientX, event.clientY);
-        calculateRotationAxis(prevCursorPosition, currentCursorPosition);
-        let v1 = prevCursorPosition;
+        currentCursorPosition = getCursorPosition(event.clientX, event.clientY);
+        calculateRotationAxis(startCursorPosition, currentCursorPosition);
+        let v1 = startCursorPosition;
         let v2 = currentCursorPosition;
         rotationAxisParagraph.innerHTML = "Rotation Axis: "+rotationAxis.x+", "+rotationAxis.y+", "+rotationAxis.z;
         cursor1Paragraph.innerHTML = "Vector1: "+v1.x+ ", "+v1.y+", "+v1.z;
         cursor2Paragraph.innerHTML = "Vector2: "+v2.x+", "+v2.y+", "+v2.z;
-        rotateObj(cube, rotationAxis, 0.1);
+        rotateObj(cube, rotationAxis, v1.distanceTo(v2));
     }
 };
 
@@ -131,7 +137,8 @@ function getObjCoord(obj) {
 };
 
 function rotateObj(obj, axis, rad) {
-    cube.rotateOnWorldAxis(axis, rad);
+    cube.quaternion.setFromAxisAngle(axis, rad);
+    //cube.rotateOnWorldAxis(axis, rad);
     resizeRenderer();
     renderer.render(scene, camera);
 };
@@ -140,7 +147,7 @@ function unprojectZ(x, y) {
     let radius = 1;
     let x2 = Math.pow(x, 2);
     let y2 = Math.pow(y, 2);
-    let radius2 = Math.pow(radius, 2)
+    let radius2 = Math.pow(radius, 2);
     if(x2+y2 <= radius2/2) {
         return Math.sqrt(radius2-(x2+y2));
     }
