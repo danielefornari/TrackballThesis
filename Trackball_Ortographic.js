35c163fd2194abf9831c53ce67d3e6c5c641d793
Soulhikari <78259633+Soulhikari@users.noreply.github.com>
2021-03-10 22:09:53 +0100

    Update Trackball_Ortographic.js

--- Trackball_Ortographic.js
+++ Trackball_Ortographic.js
@@ -45,7 +45,7 @@ const pinch = new Hammer.Pinch();
 
 singlePan.set({event: 'singlepan', pointers: 1, threshold: 0, direction: Hammer.DIRECTION_ALL});
 doublePan.set({event: 'doublepan', pointers: 2, threshold: 4, direction: Hammer.DIRECTION_ALL});
-pinch.set({threshold: 0.1});
+pinch.set({threshold: 0});
 
 manager.add([singlePan, doublePan, pinch]);
 manager.get('pinch').set({enable: true});
