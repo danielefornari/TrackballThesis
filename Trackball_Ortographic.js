34e5da399e5a05efcb517825f99f9d4dd16b1d6f
Soulhikari <78259633+Soulhikari@users.noreply.github.com>
2021-03-10 16:35:58 +0100

    Update Trackball_Ortographic.js

--- Trackball_Ortographic.js
+++ Trackball_Ortographic.js
@@ -52,6 +52,7 @@ manager.get('doublepan').recognizeWith('singlepan');    //se dal singlepan aggiu
 manager.get('doublepan').requireFailure('singlepan');
 //manager.get('pinch').recognizeWith('doublepan');
 //manager.get('doublepan').recognizeWith('pinch');
+manager.get('doublepan').recognizeWith('pinch');
 manager.get('pinch').requireFailure('doublepan');
 
 //pan gesture listeners
