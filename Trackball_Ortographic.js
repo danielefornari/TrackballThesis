e7bfaa925c4bcac8bde33a579ec36aedc362a0b0
Soulhikari <78259633+Soulhikari@users.noreply.github.com>
2021-03-14 21:56:49 +0100

    Update Trackball_Ortographic.js

--- Trackball_Ortographic.js
+++ Trackball_Ortographic.js
@@ -84,7 +84,7 @@ doublePan.set({event: 'doublepan', pointers: 2, threshold: 0, direction: Hammer.
 pinch.set({threshold: 0});  //threshold 0.05
 
 //manager.add([singlePan, doublePan, pinch, rotate]);
-manager.add([singlePan doublePan pinch]);
+manager.add([singlePan, doublePan, pinch]);
 manager.get('doublepan').recognizeWith('singlepan');    //se dal singlepan aggiungo un dito, riconosce il doublepan e continua con quello
 manager.get('pinch').recognizeWith('doublepan');
 //manager.get('pinch').recognizeWith('rotate');
