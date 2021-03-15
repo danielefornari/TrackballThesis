89d5a79de8a84dcc0793ab66f13f6cb964d20ab1
Soulhikari <78259633+Soulhikari@users.noreply.github.com>
2021-03-15 13:10:44 +0100

    Update Trackball_Ortographic.js

--- Trackball_Ortographic.js
+++ Trackball_Ortographic.js
@@ -202,20 +202,21 @@ manager.on('pinchmove', function pinchMoveListener(event) {
     p.setZ(0);
     const newDistance = calculateDistance(event.pointers[0], event.pointers[1]);
 
-    const xAxis = new THREE.Vector3(1, 0, 0);
+    /*const xAxis = new THREE.Vector3(1, 0, 0);
     const yAxis = new THREE.Vector3(0, 1, 0);
     v1.copy(obj.worldToLocal(xAxis)).multiplyScalar(p.x);
     v2.copy(obj.worldToLocal(yAxis)).multiplyScalar(p.y);
-    v1.add(v2).applyQuaternion(obj.quaternion);
+    v1.add(v2).applyQuaternion(obj.quaternion);*/
+    p.applyQuaternion(obj.quaternion);
 
     const s = newDistance/fingerDistance;
     /*obj.position.add(v1);
     scale(obj, s);
     obj.position.sub(v1);*/
-    m1.makeTranslation(v1.x, v1.y, v1.z);
+    m1.makeTranslation(p.x, p.y, p.z);
     m2.makeScale(s, s, s);
     m1.premultiply(m2);
-    m2.makeTranslation(-v1.x, -v1.y, -v1.z);
+    m2.makeTranslation(-p.x, -p.y, -p.z);
     m1.premultiply(m2);
     m2.copy(objMatrixState).premultiply(m1);
     m2.decompose(obj.position, obj.quaternion, obj.scale);
