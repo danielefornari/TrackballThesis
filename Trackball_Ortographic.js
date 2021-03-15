735c1d310a4caac7cfc99100a1a47dc7a3516166
Soulhikari <78259633+Soulhikari@users.noreply.github.com>
2021-03-15 16:52:35 +0100

    Update Trackball_Ortographic.js

--- Trackball_Ortographic.js
+++ Trackball_Ortographic.js
@@ -207,8 +207,9 @@ manager.on('pinchmove', function pinchMoveListener(event) {
     v1.copy(obj.worldToLocal(xAxis)).multiplyScalar(p.x);
     v2.copy(obj.worldToLocal(yAxis)).multiplyScalar(p.y);
     v1.add(v2).applyQuaternion(obj.quaternion);*/
-    group.getWorldQuaternion(obj.quaternion);
-    p.applyQuaternion(obj.quaternion);
+    const q = new THREE.Quaternion();
+    group.getWorldQuaternion(q);
+    p.applyQuaternion(q);
 
     const s = newDistance/fingerDistance;
     /*obj.position.add(v1);
