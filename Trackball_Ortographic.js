d74312f8f9431e45b4e96c398eac8b563a43f8a4
Soulhikari <78259633+Soulhikari@users.noreply.github.com>
2021-03-16 23:04:49 +0100

    Update Trackball_Ortographic.js

--- Trackball_Ortographic.js
+++ Trackball_Ortographic.js
@@ -206,7 +206,7 @@ function twoFingersMoveListener(event) {
     scaleMatrix.copy(m1);
 
     //rotation operation
-    const rotation = (fingerRotation - event.rotation)*Math.PI/180; //angle in radians
+    /*const rotation = (fingerRotation - event.rotation)*Math.PI/180; //angle in radians
     v1.set(p.x, 0, 0);
     v2.set(0, p.y, 0);
     v1.add(v2);
@@ -221,7 +221,7 @@ function twoFingersMoveListener(event) {
     m2.makeTranslation(v1.x, v1.y, v1.z);
     m1.multiply(m2);
     //m2.copy(objMatrixState).premultiply(m1);
-    rotateMatrix.copy(m1);
+    rotateMatrix.copy(m1);*/
 
     //panning operation
     currentCursorPosition = getCursorPosition(center.x, center.y, renderer.domElement);
@@ -238,7 +238,7 @@ function twoFingersMoveListener(event) {
     //apply matrix
     m1.copy(objMatrixState);
     m1.premultiply(scaleMatrix);
-    m1.premultiply(rotateMatrix);
+    //m1.premultiply(rotateMatrix);
     m1.premultiply(translateMatrix);
     //obj.matrix.copy(m2);
     obj.matrix.copy(m1);
