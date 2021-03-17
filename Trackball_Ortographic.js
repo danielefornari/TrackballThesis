3fb9f741dfbef3c9a5dafefb0fe6555a59de3290
Soulhikari <78259633+Soulhikari@users.noreply.github.com>
2021-03-17 13:02:38 +0100

    Update Trackball_Ortographic.js

--- Trackball_Ortographic.js
+++ Trackball_Ortographic.js
@@ -235,12 +235,12 @@ function twoFingersMoveListener(event) {
 
     //apply matrix  TRS
     m1.copy(objMatrixState);
-    /*m1.premultiply(translateMatrix);
+    m1.premultiply(translateMatrix);
     m1.premultiply(rotateMatrix);
-    m1.premultiply(scaleMatrix);*/
-    translateMatrix.multiply(rotateMatrix);
+    m1.premultiply(scaleMatrix);
+    /*translateMatrix.multiply(rotateMatrix);
     translateMatrix.multiply(scaleMatrix);
-    m1.multiply(translateMatrix);
+    m1.multiply(translateMatrix);*/
 
     obj.matrix.copy(m1);
     renderer.render(scene, camera);
